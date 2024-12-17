2ba10838bfb03421bf4987069cda5f8039855478
PCilverio <jefecitochung18.com>
2024-12-17 09:03:37 -0400

    react

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Silverio <code>Pacheco </code> Choque.
+          SILVERIO PACHECO CHOQUE.
         </p>
         <a
           className="App-link"
